05ac903ba2310c340c823fa3579e30ef2b3756bb
sakshammaggu <sakshammaggu80@gmail.com>
2025-10-02 06:13:41 +0530

    Add initial implementation of strOutputParser for generating detailed reports and summaries

---
+++ 02-LangChain/09-Output-Parsers/01-StrOutputParser/strOutputParser.py
@@ -0,0 +1,26 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+model = ChatOpenAI(model = 'gpt-4o-mini')
+
+# 1st Prompt -> detailed report
+template1 = PromptTemplate(
+    template="Write a detailed report on {topic}",
+    input_variables=["topic"],
+)
+
+# 2nd Prompt -> summarize the report in a single sentence
+template2 = PromptTemplate(
+    template="Write a 5 line summary on following text: /n {text}",
+    input_variables=["text"],
+)
+
+prompt1 = template1.invoke({"topic": "Black Hole"})
+result1 = model.invoke(prompt1)
+
+prompt2 = template2.invoke({"text": result1.content})
+result2 = model.invoke(prompt2)
+print(result2.content)
